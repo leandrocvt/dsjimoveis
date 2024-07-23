693f0994f803413f02486b9151429f5ab98e0d92
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2024-07-23 19:17:13 -0300

    Project created

---
+++ src/test/java/com/dsj/imoveis/ImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsj.imoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
